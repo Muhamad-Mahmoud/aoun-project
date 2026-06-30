23c03fdefb1620214acf0380e92cf8cdd6988e86
Muhamad-Mahmoud <mohammadmahhmod@gmail.com>
2026-06-30 20:05:51 +0300

    fix: update home barrel exports to match existing components

--- src/features/home/index.ts
+++ src/features/home/index.ts
@@ -3,9 +3,11 @@
  */
 
 export { HeroSection } from './components/HeroSection';
-export { FeaturesSection } from './components/FeaturesSection';
+export { AboutSection } from './components/AboutSection';
 export { WhyAounSection } from './components/WhyAounSection';
 export { JourneySection } from './components/JourneySection';
-export { SuccessStoriesSection } from './components/SuccessStoriesSection';
-export { PartnersSection } from './components/PartnersSection';
+export { HowItWorksSection } from './components/HowItWorksSection';
+export { ImpactSections } from './components/ImpactSections';
+export { AssociationCTASection } from './components/AssociationCTASection';
+export { StartJourneySection } from './components/StartJourneySection';
 export { FAQSection } from './components/FAQSection';
